ea115a195fd4caea586e3caaf297ae12c1f144f5
Fromero564 <fervicromero@gmail.com>
2024-10-15 14:41:43 -0300

    Se retira 4 foto

--- src/Components/Presentation.jsx
+++ src/Components/Presentation.jsx
@@ -4,7 +4,6 @@ import { Button } from 'react-bootstrap';
 import Portada1 from '../assets/img/Presentation/portada1.jpg'
 import Portada2 from '../assets/img/Presentation/portada2.jpg'
 import Portada3 from '../assets/img/Presentation/portada3.jpg'
-import Portada4 from '../assets/img/Presentation/portada4.jpg'
 
 
 
@@ -57,21 +56,7 @@ const Presentation = ()=>{
           </a>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={1000}>
-      <img
-          className="d-block w-100"
-          src={Portada4}
-          style={{ height: '100vh' }}
-          alt="Presentación SmartAqua"
-        />
-        <Carousel.Caption>
-        <h3 style={{
-      textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)'}}>TECNOLOGÍA AL SERVICIO DEL AGUA</h3> 
-         <a href="#contacto">
-          <Button variant="primary" >MÁS INFORMACIÓN</Button> 
-          </a>
-        </Carousel.Caption>
-      </Carousel.Item>
+      
     </Carousel>
         </section>
     )
